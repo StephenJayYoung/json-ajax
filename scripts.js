93957bdcfb3fc49887e3ed37b1e23ce338645719
Stephen Young <StephenJayYoung@gmail.com>
2014-10-01 10:40:47 -0700

    working, wohoo

--- scripts.js
+++ scripts.js
@@ -1,6 +1,17 @@
+// $.ajax({
+//   url: "file.json",
+//   context: document.body
+// }).done(function() {
+// 	console.log('done');
+//   $( this ).addClass( "done" );
+// });
+
+// Using the core $.ajax() method
 $.ajax({
-  url: "index.html",
-  context: document.body
-}).done(function() {
-  $( this ).addClass( "done" );
+    url: "file.json",
+    dataType : "json",
+    success: function( json ) {
+    	console.log(json);
+      $( "<h1/>" ).text( json.glossary.title ).appendTo( "body" );
+    }
 });
